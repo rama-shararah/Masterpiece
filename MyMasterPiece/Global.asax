650f8525d9caee4e5d1489c5726618625125d951
rama-shararah <115452637+rama-shararah@users.noreply.github.com>
2023-05-08 01:08:44 +0300

    update

---
+++ MyMasterPiece/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyMasterPiece.MvcApplication" Language="C#" %>
---
+++ MyMasterPiece/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMasterPiece.Startup))]
+namespace MyMasterPiece
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
